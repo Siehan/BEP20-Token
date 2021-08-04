205474d1e891c01a861229b57f5aa63d93b57267
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-08-04 02:27:39 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,8 @@ require('@nomiclabs/hardhat-solhint');
 require('hardhat-docgen');
 require('dotenv').config();
 
-const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
-const DEPLOYER_PRIVATE_KEY = process.env.PRIVATE_KEY;
-
+//const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -13,28 +12,8 @@ const DEPLOYER_PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: '0.8.5',
   networks: {
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
-    },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
-    },
     testnet: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org/',
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
       chainId: 97,
       gasPrice: 20000000000,
       accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
