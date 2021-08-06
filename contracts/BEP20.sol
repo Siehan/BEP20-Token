858219603bd63703870e39b156ade4d7e2eb28a2
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-08-06 22:04:13 +0200

    Update BEP20.sol

--- contracts/BEP20.sol
+++ contracts/BEP20.sol
@@ -3,10 +3,8 @@
 pragma solidity ^0.8.5;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./IBEP20.sol";
 
 /// @title BEP20 Token
-/// @author Binance Chain
 
 contract BEP20 is ERC20 {
     constructor(uint256 totalSupply_) ERC20("BEP20", "BEP") {
